83739503cd5d6ea60314e6627f4d7c16d1c408ce
balogun biola <balogunbiola101@gmail.com>
2024-08-03 15:44:04 +0100

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -46,7 +46,8 @@ export class AuthService {
         }
         const decryptedPassword = useEncryptionService().decryptData(password, this.configService.get('ENCRYPTION_KEY'))
 
-      
+
+        console.log('11111111111')
         const user = await this.userService.findOne({
             field: USER.EMAIL,
             data: email,
@@ -58,6 +59,7 @@ export class AuthService {
             password: decryptedPassword ? await bcrypt.hash(decryptedPassword, 10) : await bcrypt.hash(password, 10),
             referral_code
         })
+        console.log('22222222222')
           console.log(`decrypted pass - ${decryptedPassword}`, user)
         if (user.verified) returnErrorResponse('Already a user')
 
